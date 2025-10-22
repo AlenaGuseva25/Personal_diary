e257470481816fd44f8c9874962da72fbb2885f0
Alena <alittlekindness94@gmail.com>
2025-10-22 14:25:56 +0500

    регистрация

--- diary/models.py
+++ diary/models.py
@@ -6,11 +6,6 @@ class DiaryEntry(models.Model):
     '''Модель записи'''
     title = models.CharField(max_length=150, verbose_name="Заголовок")
     content = models.TextField(verbose_name='Содержание')
-    picture = models.ImageField(blank=True,
-                                null=True,
-                                upload_to='diary_pictures/%Y/%m/%d/',
-                                verbose_name='Изображение',
-                                help_text='Прикрепите фото для вашей записи')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True ,verbose_name='Дата изменения')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Автор')
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'is_active', 'is_staff', 'date_joined')
+    list_filter = ('is_active', 'is_staff', 'date_joined')
+    list_display_links = ('email',)
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Верификация', {'fields': ('verification_token',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
